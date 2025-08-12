76a116452995b59a29ee15f205a87cfeeeb794dc
Aditi Sethi <aditisethi@aditis-mbp-3.lan>
2025-08-11 23:01:15 -0700

    adding script to html

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ function getDogImage() {
     fetch("https://dog.ceo/api/breeds/image/random")
       .then(response => response.json())
       .then(data => {
-        document.getElementById("dogImage").src = data.message;
+        const img = document.getElementById("dogImage");
+        img.src = data.message;
+        img.alt = "Cute dog";
       })
       .catch(error => {
-        alert("Something went wrong: " + error);
+        alert("Something went wrong: " + error.message);
+        console.error(error);
       });
   }
